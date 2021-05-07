1ea6ed2fee3545eb1c1485412bf389780de93973
kitanovski-dev <kitanovski.development@gmail.com>
2021-05-07 22:56:40 +0200

    Changed controllers location and namespaces.

--- app/Http/Controllers/API/Auth/ReviewController.php
+++ app/Http/Controllers/API/Auth/ReviewController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\API\Auth;
 
 use App\Models\Review;
 use Illuminate\Http\Request;
--- routes/api.php
+++ routes/api.php
@@ -4,8 +4,8 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\LoginController;
 use App\Http\Controllers\API\LogoutController;
-use App\Http\Controllers\API\ProductController;
-use App\Http\Controllers\API\RegisterController;
+use App\Http\Controllers\API\Auth\ProductController;
+use App\Http\Controllers\API\Auth\RegisterController;
 use Symfony\Component\HttpKernel\DependencyInjection\RegisterControllerArgumentLocatorsPass;
 
 /*
@@ -26,11 +26,8 @@ Route::group(['namespace' => 'API'], function () {
 });
 
 Route::group(['middleware' => 'auth:api'], function () {
-    Route::group(['namespace' => 'API'], function () {
+    Route::group(['namespace' => 'API\Auth'], function () {
         Route::get('/products', [ProductController::class, 'index']);
         Route::post('/products', [ProductController::class, 'store']);
     });
 });
-
-// Route::middleware('auth:api')->group(['namespance' => 'API\User'], function () {
-    // });z
